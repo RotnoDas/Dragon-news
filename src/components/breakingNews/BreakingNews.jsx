2c779ab888a2cb044ad135ba3bfefcffb0c8c471
Rotno Chandra Das <rotno050@gmail.com>
2026-06-04 00:39:01 +0600

    Breaking news added

--- src/components/breakingNews/BreakingNews.jsx
+++ src/components/breakingNews/BreakingNews.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
 
+const news = [
+    {
+        id: "1",
+        title: "Breaking News: Major Event Unfolds in the City",
+    },
+    {
+        id: "2",
+        title: "Breaking News: New Policy Announced by the Government",
+    },
+    {
+        id: "3",
+        title: "Breaking News: Sports Team Wins Championship",
+    },
+];
+
 const BreakingNews = () => {
     return (
         <div className='px-37.5'>
@@ -10,8 +25,14 @@ const BreakingNews = () => {
                         <button className='bg-[#D72050] py-2.25 px-6 poppins font-medium text-[20px] leading-7.5 tracking-[0%] text-[#FFFFFF]'>Latest</button>
                     </div>
                     <div>
-                        <Marquee>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut laudantium vel sed voluptatum, sit facere ipsam magnam pariatur, voluptates deserunt veniam ullam, mollitia illum nostrum placeat eos consequatur. Non, adipisci.
+                        <Marquee pauseOnHover={true} speed={100} gradient={false}>
+                            {
+                                news.map((item) => {
+                                    return(
+                                        <p key={item.id} className='poppins font-semibold text-[18px] leading-7.5 tracking-[0%] text-[#403F3F] px-4'>{item.title}</p>
+                                    )
+                                })
+                            }
                         </Marquee>
                     </div>
                 </div>
